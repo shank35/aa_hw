b654aabb09b92030463eb918d36a95752cd6ea0c
Shan Kim <121928503+shank35@users.noreply.github.com>
2023-01-27 02:04:00 -0500

    Add files via upload

---
+++ w3d5/DIY-ADTs.rb
@@ -0,0 +1,92 @@
+#Exercise 1
+class Stack
+    def initialize
+      @stack_array = []
+    end
+
+
+    def push(el)
+      stack_array.push(el)
+      el
+    end
+
+    def pop
+      stack_array.pop(el)
+    end
+
+    def peek
+      stack_array.last
+    end
+
+    private
+    attr_reader :stack_array
+end
+
+#Exercise 2
+class Queue
+  def initialize
+    @array = []
+  end
+
+  def enqueue(el)
+    array.push(el)
+    el
+  end
+
+  def dequeue
+    array.shift
+  end
+
+  def peek
+    array.first
+  end
+
+  private
+
+  attr_reader :array
+  
+end
+
+# EXERCISE 3
+class Map
+  def initialize
+    @array = []
+  end
+
+  def set(key, value)
+    pair = array.index { |pair| pair[0] == key }
+    if pair
+      array[pair][1] = value
+    else
+      array.push([key, value])
+    end
+    value
+  end
+
+  def get(key)
+    array.each do |pair| 
+      if pair[0] == key
+        return pair[1] 
+      end
+    end
+    nil
+  end
+
+  def delete(key)
+    value = get(key)
+    array.reject! { |pair| pair[0] == key }
+    value
+  end
+
+  def show
+    deep_dup(array)
+  end
+
+  private
+
+  attr_reader :array
+  
+  def deep_dup(arr)
+    arr.map { |el| el.is_a?(Array) ? deep_dup(el) : el }
+  end  
+end
