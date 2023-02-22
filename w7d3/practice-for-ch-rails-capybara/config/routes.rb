d12d2786f9fa31c3d3650ce69b0c4f6db4ae1465
Shan Kim <121928503+shank35@users.noreply.github.com>
2023-02-21 22:54:10 -0500

    Add files via upload

---
+++ w7d3/practice-for-ch-rails-capybara/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "bands#index"
+
+  resource :session, only: [:new, :create, :destroy]
+
+  resources :users, only: [:index, :show, :new, :create]
