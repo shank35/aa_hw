76a9370b94d96d0840bfb8deb46c40eb71e2bb04
Shan Kim <121928503+shank35@users.noreply.github.com>
2023-02-28 06:07:36 -0500

    Add files via upload

---
+++ w8d1/part_i.js
@@ -0,0 +1,113 @@
+function mysteryScoping1() {
+  var x = 'out of block';
+  if (true) {
+    var x = 'in block';
+    console.log(x);
+  }
+  console.log(x);
+}
+
+mysteryScoping1(); 
+// in block, in block
+
+function mysteryScoping2() {
+  const x = 'out of block';
+  if (true) {
+    const x = 'in block';
+    console.log(x);
+  }
+  console.log(x);
+}
+
+mysteryScoping2(); 
+// in block, out of block
+
+function mysteryScoping3() {
+  const x = 'out of block';
+  if (true) {
+    var x = 'in block';
+    console.log(x);
+  }
+  console.log(x);
+}
+
+mysteryScoping3(); 
+// syntaxerror
+
+function mysteryScoping4() {
+  let x = 'out of block';
+  if (true) {
+    let x = 'in block';
+    console.log(x);
+  }
+  console.log(x);
+}
+
+mysteryScoping4(); 
+// in block, out of block
+
+function mysteryScoping5() {
+  let x = 'out of block';
+  if (true) {
+    let x = 'in block';
+    console.log(x);
+  }
+  let x = 'out of block again';
+  console.log(x);
+}
+
+mysteryScoping5(); 
+// syntaerror
+
+function madLib(verb, adj, noun) {
+  return `We shall ${verb.toUpperCase()} the ${adj.toUpperCase()} ${noun.toUpperCase()}.`
+}
+
+
+function fizzBuzz (array) {
+  const fizzBuzzArr = [];
+
+  array.forEach(el => {
+    if ((el % 3 === 0) ^ (el % 5 === 0)) {
+      fizzBuzzArr.push(el);
+    }
+  });
+
+  return fizzBuzzArr;
+}
+
+// isPrime
+
+function isPrime (n) {
+  if (n < 2) { return false; }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function sumOfNPrimes (n) {
+  let sum = 0;
+  let countPrimes = 0;
+  let i = 2;
+
+  while (countPrimes < n) {
+    if (isPrime(i)) {
+      sum += i;
+      countPrimes++;
+    }
+    i++;
+  }
+
+  return sum;
+}
+
+function titleize(names, callback) {
+  let titleized = names.map(name => `Mx. ${name} Jingleheimer Schmidt`);
+  callback(titleized);
+};
+
