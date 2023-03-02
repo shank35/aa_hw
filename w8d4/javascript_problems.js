69fea3d8516b7a6452aa0d1cf605858572f7acb7
Shan Kim <121928503+shank35@users.noreply.github.com>
2023-03-02 08:40:59 -0500

    Add files via upload

---
+++ w8d4/javascript_problems.js
@@ -0,0 +1,23 @@
+function titleize(names, callback) {
+  let titleized = names.map(name => `Mx. ${name} Jingleheimer Schmidt`);
+  callback(titleized);
+};
+
+
+function Elephant(name, height, tricks) {
+  this.name = name;
+  this.height = height;
+  this.tricks = tricks;
+}
+
+Elephant.prototype.trumpet = function () {
+  console.log(`${this.name} the elephant goes 'phrRRRRRRR!!!!!!!'`);
+};
+
+Elephant.prototype.grow = function () {
+  this.height = this.height + 12;
+};
+
+Elephant.prototype.addTrick = function (trick) {
+  this.tricks.push(trick);
+};
